49cbbe8fb4e9c2f9dc60c2e03c8742639821f68a
Elaine JY Kim <elainejykim@Elaines-MacBook-Pro.local>
2021-05-20 20:11:59 +0900

    media queries for responsiveness

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Link, withRouter } from "react-router-dom";
 
 export default function ComingSoon() {
     return (
-        <div>
-            <div class="container">
-                <h1>
+        <div class="coming-soon">
+            <div class="container p-4">
+                <h1 class="lg">
                     COMING SOON!
                 </h1>
+
+                <h2 class="d-txt md">
+                    In the meantime... 
+                </h2>
+
+                <button class="d-txt btn">
+                    <Link to="/submit-event" class="d-txt">Submit Event</Link>
+                </button>
+                
             </div>
         </div>
     )
